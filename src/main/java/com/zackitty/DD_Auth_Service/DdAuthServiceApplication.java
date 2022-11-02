82854d96e22b3071b67fd9b93f3a0c6f0b4a03f5
zackitty <theelfenliedpictureshow@yahoo.com>
2022-11-02 13:33:40 -0500

    init

---
+++ src/main/java/com/zackitty/DD_Auth_Service/DdAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zackitty.DD_Auth_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zackitty/DD_Auth_Service/DdAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zackitty.DD_Auth_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
